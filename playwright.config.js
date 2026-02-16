5d9ac6f8a550b1a6f2c4f6e83c50352371d480b5
Sonu Katkar <sonu.katkar@dhlscontractors.com>
2026-02-16 17:40:15 +0530

    Update .gitignore and playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -29,8 +29,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { outputFolder: 'regression-report', open: 'never' }],
-    ['junit', { outputFile: 'regression-results.xml' }], ['list']],
+  reporter: [['html', { outputFolder: 'regression-report', open: 'never' }],['list']],
  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
