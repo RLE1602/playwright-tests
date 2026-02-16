717eded5d5eea34b37cfc81c292afbf6690c06ae
Sonu Katkar <sonu.katkar@dhlscontractors.com>
2026-02-16 20:47:12 +0530

    CoreScenarios

---
+++ tests/Regression/CoreScenarios/Portugal-05.spec.js
@@ -0,0 +1,40 @@
+import { test, expect } from '@playwright/test';
+test.afterEach(async ({ page }, testInfo) => {
+  await testInfo.attach('Full Page Screenshot', { body: await page.screenshot({ fullPage: true }),contentType: 'image/png',});
+});
+
+test('Portugal-05 Existing customer RFQ (Request for Quote) submission', async ({ page }) => {
+  await page.goto('https://stage10.phenomenex.com/');
+  await page.getByRole('button', { name: 'Accept All Cookies' }).click();
+  await page.getByRole('link', { name: 'Sign In' }).click();
+
+  await page.getByRole('textbox', { name: 'Email Address' }).fill('portugal_user2@yopmail.com');
+
+  await page.getByRole('textbox', { name: 'Password' }).fill('Welcome@123');
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  
+  await expect(page).toHaveURL('https://stage10.phenomenex.com/', { waitUntil: 'load', timeout: 200_000});
+  await page.getByRole('textbox', { name: 'Search by Part No., Product,' }).click();
+  await page.locator('textarea').fill('00B-4441-B0');
+  await page.keyboard.press('Enter');
+  await page.getByRole('button', { name: 'Add To Quote' }).nth(0).click();
+  await page.getByRole('button', { name: 'Add To Quote' }).nth(0).click();
+  await page.getByRole('textbox', { name: 'Search by Part No., Product,' }).click();
+  await page.locator('//*[@id="holder"]//app-header-search-modal//span[2]/i').click();
+  await page.getByRole('textbox', { name: 'Search by Part No., Product,' }).nth(0).click();
+
+  await page.locator('textarea').nth(0).fill('00B-4446-B0');
+  await page.keyboard.press('Enter');
+  await page.getByRole('button', { name: 'Add To Quote' }).nth(0).click();
+  await page.locator("xpath=//a[@class='btn btn-link with-icon px-2 ng-star-inserted']").click();
+  //await page.locator('link', { name: 'Quote shopping_cart' }).click();
+  await page.waitForURL(/quote\.html/, { waitUntil: 'domcontentloaded' });
+  
+  // Submit Quote Request
+  await page.getByRole('button', { name: 'Submit Quote Request' }).click();
+  
+  // Verify successful submission
+  await expect(page).toHaveURL(/.*confirmation|success|thankyou.*/i);
+  await expect(page.locator('text=Quote submitted successfully')).toBeVisible();
+
+});
---
+++ tests/Regression/CoreScenarios/Portugal-06.spec.js
@@ -0,0 +1,50 @@
+import { test, expect } from '@playwright/test';
+test.afterEach(async ({ page }, testInfo) => {
+  await testInfo.attach('Full Page Screenshot', { body: await page.screenshot({ fullPage: true }),contentType: 'image/png',});
+});
+
+test('Portugal-06 Existing customer order from approved quote, payment method-credit card(Amex)', async ({ page }) => {
+  await page.goto('https://stage10.phenomenex.com/');
+  await page.getByRole('button', { name: 'Accept All Cookies' }).click();
+  await page.getByRole('link', { name: 'Sign In' }).click();
+  await page.getByRole('textbox', { name: 'Email Address' }).fill('Portugal_stage_fbgj@mailsac.com');
+  await page.getByRole('textbox', { name: 'Password' }).fill('Welcome@123');
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await expect(page).toHaveURL('https://stage10.phenomenex.com/', { waitUntil: 'load', timeout: 200_000});
+  await page.getByRole('button', { name: 'Welcome AutoFirstName' }).click();  
+  await page.locator('span').filter({ hasText: /^Quotes$/ }).click();
+
+
+  const noData = page.getByText('No data available');
+  if (await noData.isVisible()) {
+  console.log('No Quotes available');}
+  else {
+  // execute checkout flow
+
+  await page.getByRole('button', { name: 'Quote No: ES1267044 Expires:' }).getByRole('button').click();
+  await page.getByRole('button', { name: 'Go To Cart' }).click();
+  await page.waitForURL(/cart\.html/, { waitUntil: 'domcontentloaded' });
+
+  await page.getByRole('button', { name: 'Checkout' }).click();
+  await page.waitForURL(/addresses\.html/, { waitUntil: 'domcontentloaded' });
+
+  await page.getByRole('button', { name: 'Proceed to Shipping Method' }).click();
+  await page.waitForURL(/shipping\.html/, { waitUntil: 'domcontentloaded' });
+
+  //await page.goto('https://stage-shop.phenomenex.com/au/en/shipping.html');
+  await page.getByRole('button', { name: 'Proceed to Payment' }).click();
+  await page.waitForURL(/payment\.html/, { waitUntil: 'domcontentloaded' });
+
+  await page.evaluate(() => { window.scrollBy(0, 500);});
+  //await page.getByText('Use Card').nth(0).click();
+  //await page.getByRole('button', { name: 'Use Card' }).nth(0).click();
+  await page.evaluate(() => { window.scrollBy(0, 700);});
+  await page.getByRole('checkbox').scrollIntoViewIfNeeded();
+
+  await page.locator('(//input[@id="accept-term"])[2]').check();
+  await page.getByRole('button', { name: 'Place your order' }).click();
+  await page.waitForURL(/receipt\.html/, { waitUntil: 'domcontentloaded' });
+  await expect(page).toHaveURL(/^https:\/\/stage-shop\.phenomenex\.com\/eu\/en\/receipt\.html/);
+}
+
+});
