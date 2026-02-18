2810deec2d50c3f97dd49af506927c8668d9f9ac
Sonu Katkar <sonu.katkar@dhlscontractors.com>
2026-02-18 17:12:24 +0530

    AdditionalScenarios

--- tests/Regression/AdditionalScenarios/Portugal-09.spec.js
+++ tests/Regression/AdditionalScenarios/Portugal-09.spec.js
@@ -49,7 +49,7 @@ test('Portugal-09 Complete the checkout flow until the Order Submission page, re
   await page.getByText('Edit').nth(3).click();
   await page.getByText('Cancel').click();
   await page.getByRole('button', { name: 'Use Address' }).nth(0).click();
-  await expect(page.getByRole('button', { name: 'Use Address' })).toBeHidden();
+  await expect(page.getByRole('button', { name: 'Use Address' })).nth(0).toBeHidden();
   await page.getByRole('button', { name: 'Proceed to Shipping Method' }).click();
   await page.waitForURL(/shipping\.html/, { waitUntil: 'domcontentloaded' });
   await page.getByText('Day Express Saver +55,00 €').click();
