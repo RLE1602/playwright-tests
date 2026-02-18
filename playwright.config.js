b5c6a86b9574192e8388ff1d318b04eb682f72ea
Sonu Katkar <sonu.katkar@dhlscontractors.com>
2026-02-18 16:53:53 +0530

    playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
   //retries:2,
 
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 3 : 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html', { outputFolder: 'regression-report', open: 'never' }],['list']],
  
