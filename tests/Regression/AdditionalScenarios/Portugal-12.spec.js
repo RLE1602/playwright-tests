cc32f9601e9a5a64728a0b4419e42efcfc40e498
Sonu Katkar <sonu.katkar@dhlscontractors.com>
2026-02-19 19:28:50 +0530

    AdditionalScenarios

--- tests/Regression/AdditionalScenarios/Portugal-12.spec.js
+++ tests/Regression/AdditionalScenarios/Portugal-12.spec.js
@@ -4,7 +4,7 @@ test.afterEach(async ({ page }, testInfo) => {
 });
 
 test('Portugal-12 Complete the checkout flow until the Payment page, return to the Cart to update items, addresses, and shipping/payment methods, verify all changes, and then place the order', async ({ page }) => {
-  //Login
+
   await page.goto('https://stage10.phenomenex.com/');
   await page.getByRole('button', { name: 'Accept All Cookies' }).click();
   await page.getByRole('link', { name: 'Sign In' }).click();
@@ -21,45 +21,73 @@ test('Portugal-12 Complete the checkout flow until the Payment page, return to t
   await page.getByRole('button', { name: 'Add To Cart' }).nth(0).click();
   await page.getByRole('button', { name: 'Continue Shopping' }).click();
   await page.getByRole('textbox', { name: 'Search by Part No., Product,' }).click();
+
   await page.locator('//*[@id="holder"]//app-header-search-modal//span[2]/i').click();
   await page.getByRole('textbox', { name: 'Search by Part No., Product,' }).nth(0).click();
+
   await page.locator('textarea').nth(0).fill('00B-4446-B0');
   await page.keyboard.press('Enter');
   await page.getByRole('button', { name: 'Add To Cart' }).nth(0).click();
   await page.getByRole('button', { name: 'Continue Shopping' }).click();
   await page.getByRole('link', { name: 'Cart shopping_cart' }).click();
   await page.waitForURL(/cart\.html/, { waitUntil: 'domcontentloaded' });
+
   await page.getByRole('button', { name: 'Checkout' }).click();
   await page.waitForURL(/addresses\.html/, { waitUntil: 'domcontentloaded' });
+
   await page.getByRole('button', { name: 'Proceed to Shipping Method' }).click();
   await page.waitForURL(/shipping\.html/, { waitUntil: 'domcontentloaded' });
+
   //await page.goto('https://stage-shop.phenomenex.com/au/en/shipping.html');
   await page.getByRole('button', { name: 'Proceed to Payment' }).click();
   await page.waitForURL(/payment\.html/, { waitUntil: 'domcontentloaded' });
-  // Test case 09 is begun
-  await page.getByRole('link', { name: 'Cart' }).click();
-  await page.waitForURL(/cart\.html/, { waitUntil: 'domcontentloaded' });
 
+//Test case 01 is begun 
+  //await page.getByRole('button', { name: 'Cart shopping_cart' }).click();
+  await page.locator('//span[normalize-space()="Cart"]').click();
+  await page.waitForURL(/cart\.html/, { waitUntil: 'domcontentloaded' });
   await page.getByRole('textbox').nth(0).click();
-  await page.getByRole('textbox').fill('5');
-  await page.getByRole('textbox').press('Enter');
+  await page.getByRole('textbox').nth(0).fill('5');
+  await page.getByRole('textbox').nth(0).press('Enter');
   await page.getByRole('button', { name: 'Checkout' }).click();
   await page.waitForURL(/addresses\.html/, { waitUntil: 'domcontentloaded' });
-  await page.getByText('Edit / Change').first().click();
-  await page.getByText('Edit').nth(3).click();
-  await page.getByText('Cancel').click();
-  await page.getByRole('button', { name: 'Use Address' }).nth(0).click();
-  await expect(page.getByRole('button', { name: 'Use Address' })).toBeHidden();
+  await page.locator('//div[@class="flex flex-col w-2/3"]//a[@class="font-bold cursor-pointer text-danaherpurple-500"]').click();
+  await page.getByRole('button', { name: 'Use Address' }).first().click();
+  await expect(page.getByRole('button', { name: 'Use Address' }).first()).toBeHidden();
+
   await page.getByRole('button', { name: 'Proceed to Shipping Method' }).click();
-  await page.waitForURL(/shipping\.html/, { waitUntil: 'domcontentloaded' });
-  await page.getByText('Day Express Saver +55,00 €').click();
+  await page.waitForURL(/shipping\.html/, { waitUntil: 'domcontentloaded' });  
+  await page.getByText(/Day Express Saver/i).click();
+
+
+  //await page.goto('https://stage-shop.phenomenex.com/au/en/shipping.html');
   await page.getByRole('button', { name: 'Proceed to Payment' }).click();
   await page.waitForURL(/payment\.html/, { waitUntil: 'domcontentloaded' });
-  await page.evaluate(() => { window.scrollBy(0, 700);});
-  await page.getByRole('checkbox').scrollIntoViewIfNeeded();
-  await page.locator('(//input[@id="accept-term"])[2]').check();
-  await page.getByRole('button', { name: 'Place your order' }).click();
-  await page.waitForURL(/receipt\.html/, { waitUntil: 'domcontentloaded' });
-  await expect(page).toHaveURL(/^https:\/\/stage-shop\.phenomenex\.com\/eu\/en\/receipt\.html/);
+
+  await page.evaluate(() => { window.scrollBy(0, 500);});
+  //await page.getByText('Use Card').nth(0).click();
+  
+  const useCardButtons = page.getByText('Use Card');
+
+   // Wait for buttons to be visible
+   await expect(useCardButtons.nth(0)).toBeVisible({ timeout: 10000 });
+   await expect(useCardButtons.nth(1)).toBeVisible({ timeout: 10000 });
+
+  // Click logic
+  if (!(await useCardButtons.nth(0).isEnabled())) {
+  // First card not clickable → click second
+   await useCardButtons.nth(1).click();
+    } else {
+  // First card clickable → click first
+   await useCardButtons.nth(0).click();}
+
+   await page.evaluate(() => { window.scrollBy(0, 700);});
+   await page.getByRole('checkbox').scrollIntoViewIfNeeded();
+
+   await page.locator('(//input[@id="accept-term"])[2]').check();
+   await page.getByRole('button', { name: 'Place your order' }).click();
+   await page.waitForURL(/receipt\.html/, { waitUntil: 'domcontentloaded' });
+   await expect(page).toHaveURL(/^https:\/\/stage-shop\.phenomenex\.com\/eu\/en\/receipt\.html/);
+   await expect(page.getByText(/Order Confirmed/i)).toBeVisible();
 
 });
--- tests/Regression/AdditionalScenarios/Portugal-13.spec.js
+++ tests/Regression/AdditionalScenarios/Portugal-13.spec.js
@@ -4,7 +4,7 @@ test.afterEach(async ({ page }, testInfo) => {
 });
 
 test('Portugal-13 Payment Method Switch Validation (Complete the checkout flow until the Payment page, switch the payment method, return to the Cart to update items, verify payment changes persist, and then place the order)', async ({ page }) => {
-  //Login
+
   await page.goto('https://stage10.phenomenex.com/');
   await page.getByRole('button', { name: 'Accept All Cookies' }).click();
   await page.getByRole('link', { name: 'Sign In' }).click();
@@ -21,47 +21,78 @@ test('Portugal-13 Payment Method Switch Validation (Complete the checkout flow u
   await page.getByRole('button', { name: 'Add To Cart' }).nth(0).click();
   await page.getByRole('button', { name: 'Continue Shopping' }).click();
   await page.getByRole('textbox', { name: 'Search by Part No., Product,' }).click();
+
   await page.locator('//*[@id="holder"]//app-header-search-modal//span[2]/i').click();
   await page.getByRole('textbox', { name: 'Search by Part No., Product,' }).nth(0).click();
+
   await page.locator('textarea').nth(0).fill('00B-4446-B0');
   await page.keyboard.press('Enter');
   await page.getByRole('button', { name: 'Add To Cart' }).nth(0).click();
   await page.getByRole('button', { name: 'Continue Shopping' }).click();
   await page.getByRole('link', { name: 'Cart shopping_cart' }).click();
   await page.waitForURL(/cart\.html/, { waitUntil: 'domcontentloaded' });
+
   await page.getByRole('button', { name: 'Checkout' }).click();
   await page.waitForURL(/addresses\.html/, { waitUntil: 'domcontentloaded' });
+
   await page.getByRole('button', { name: 'Proceed to Shipping Method' }).click();
   await page.waitForURL(/shipping\.html/, { waitUntil: 'domcontentloaded' });
+
   //await page.goto('https://stage-shop.phenomenex.com/au/en/shipping.html');
   await page.getByRole('button', { name: 'Proceed to Payment' }).click();
   await page.waitForURL(/payment\.html/, { waitUntil: 'domcontentloaded' });
-  await page.evaluate(() => { window.scrollBy(0, 500);});
-  await page.getByText('Use Card').nth(0).click();
-  // Test case 09 is begun
-  await page.getByRole('link', { name: 'Cart' }).click();
-  await page.waitForURL(/cart\.html/, { waitUntil: 'domcontentloaded' });
+  await expect(useCardButtons.nth(0)).toBeVisible({ timeout: 10000 });
+   await expect(useCardButtons.nth(1)).toBeVisible({ timeout: 10000 });
 
+  // Click logic
+  if (!(await useCardButtons.nth(0).isEnabled())) {
+  // First card not clickable → click second
+   await useCardButtons.nth(1).click();
+    } else {
+  // First card clickable → click first
+   await useCardButtons.nth(0).click();}
+
+//Test case 01 is begun 
+  //await page.getByRole('button', { name: 'Cart shopping_cart' }).click();
+  await page.locator('//span[normalize-space()="Cart"]').click();
+  await page.waitForURL(/cart\.html/, { waitUntil: 'domcontentloaded' });
   await page.getByRole('textbox').nth(0).click();
-  await page.getByRole('textbox').fill('5');
-  await page.getByRole('textbox').press('Enter');
+  await page.getByRole('textbox').nth(0).fill('5');
+  await page.getByRole('textbox').nth(0).press('Enter');
   await page.getByRole('button', { name: 'Checkout' }).click();
   await page.waitForURL(/addresses\.html/, { waitUntil: 'domcontentloaded' });
-  await page.getByText('Edit / Change').first().click();
-  await page.getByText('Edit').nth(3).click();
-  await page.getByText('Cancel').click();
-  await page.getByRole('button', { name: 'Use Address' }).nth(0).click();
-  await expect(page.getByRole('button', { name: 'Use Address' })).toBeHidden();
+
   await page.getByRole('button', { name: 'Proceed to Shipping Method' }).click();
-  await page.waitForURL(/shipping\.html/, { waitUntil: 'domcontentloaded' });
-  await page.getByText('Day Express Saver +55,00 €').click();
+  await page.waitForURL(/shipping\.html/, { waitUntil: 'domcontentloaded' });  
+
+  //await page.goto('https://stage-shop.phenomenex.com/au/en/shipping.html');
   await page.getByRole('button', { name: 'Proceed to Payment' }).click();
   await page.waitForURL(/payment\.html/, { waitUntil: 'domcontentloaded' });
-  await page.evaluate(() => { window.scrollBy(0, 700);});
-  await page.getByRole('checkbox').scrollIntoViewIfNeeded();
-  await page.locator('(//input[@id="accept-term"])[2]').check();
-  await page.getByRole('button', { name: 'Place your order' }).click();
-  await page.waitForURL(/receipt\.html/, { waitUntil: 'domcontentloaded' });
-  await expect(page).toHaveURL(/^https:\/\/stage-shop\.phenomenex\.com\/eu\/en\/receipt\.html/);
+
+  await page.evaluate(() => { window.scrollBy(0, 500);});
+  //await page.getByText('Use Card').nth(0).click();
+  
+  const useCardButtons = page.getByText('Use Card');
+
+   // Wait for buttons to be visible
+   await expect(useCardButtons.nth(0)).toBeVisible({ timeout: 10000 });
+   await expect(useCardButtons.nth(1)).toBeVisible({ timeout: 10000 });
+
+  // Click logic
+  if (!(await useCardButtons.nth(0).isEnabled())) {
+  // First card not clickable → click second
+   await useCardButtons.nth(1).click();
+    } else {
+  // First card clickable → click first
+   await useCardButtons.nth(0).click();}
+
+   await page.evaluate(() => { window.scrollBy(0, 700);});
+   await page.getByRole('checkbox').scrollIntoViewIfNeeded();
+
+   await page.locator('(//input[@id="accept-term"])[2]').check();
+   await page.getByRole('button', { name: 'Place your order' }).click();
+   await page.waitForURL(/receipt\.html/, { waitUntil: 'domcontentloaded' });
+   await expect(page).toHaveURL(/^https:\/\/stage-shop\.phenomenex\.com\/eu\/en\/receipt\.html/);
+   await expect(page.getByText(/Order Confirmed/i)).toBeVisible();
 
 });
