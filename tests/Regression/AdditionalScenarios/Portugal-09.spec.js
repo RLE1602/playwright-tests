443ffc7c9512a46bab3409f7ef63e70788037334
Sonu Katkar <sonu.katkar@dhlscontractors.com>
2026-02-19 13:10:09 +0530

    AdditionalScenarios

--- tests/Regression/AdditionalScenarios/Portugal-09.spec.js
+++ tests/Regression/AdditionalScenarios/Portugal-09.spec.js
@@ -48,9 +48,9 @@ test('Portugal-09 Complete the checkout flow until the Order Submission page, re
   await page.waitForURL(/addresses\.html/, { waitUntil: 'domcontentloaded' });
   await page.locator('//div[@class="flex flex-col w-2/3"]//a[@class="font-bold cursor-pointer text-danaherpurple-500"]').click();
   //await page.getByRole('button', { name: 'Edit / Change' }).click();
-  await page.getByRole('button', { name: 'Edit', visible: true }).click();
+  //await page.getByRole('button', { name: 'Edit', visible: true }).click();
   //await page.getByRole('button', { name: 'Edit' }).first().click();
-  await page.getByRole('button', { name: 'Cancel' }).click();
+  //await page.getByRole('button', { name: 'Cancel' }).click();
   await page.getByRole('button', { name: 'Use Address' }).first().click();
   await expect(page.getByRole('button', { name: 'Use Address' }).first()).toBeHidden();
   await page.getByRole('button', { name: 'Proceed to Shipping Method' }).scrollIntoViewIfNeeded();
