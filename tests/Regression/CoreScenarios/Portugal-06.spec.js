09d1ae641e366471be4069aaab10ec962a1ad0e7
Sonu Katkar <sonu.katkar@dhlscontractors.com>
2026-02-20 01:20:48 +0530

    Regression

--- tests/Regression/CoreScenarios/Portugal-06.spec.js
+++ tests/Regression/CoreScenarios/Portugal-06.spec.js
@@ -10,15 +10,52 @@ test('Portugal-06 Existing customer order from approved quote, payment method-cr
   await expect(page).toHaveURL('https://stage10.phenomenex.com/', { waitUntil: 'load', timeout: 200_000 });
   await page.getByRole('button', { name: 'Welcome AutoFirstName' }).click();  
   await page.locator('span').filter({ hasText: /^Quotes$/ }).click();
-  await expect(page).toHaveURL(/quotes/, { waitUntil: 'load', timeout: 200_000});
+  await expect(page).toHaveURL(/quotes/, { waitUntil: 'domcontentloaded' });
 
-  //const noDataElement = page.getByText('No Data');
-  const noDataElement = await page.getByRole('button', { name: 'REQUEST A QUOTE' }).isVisible();
+
+  // Wait until Quotes page fully loads
+  await page.waitForLoadState('domcontentloaded');
+
+  const noData = page.getByRole('link', { name: 'REQUEST A QUOTE' });
+
+  if (await noData.isVisible()) {
+  await expect(noData).toBeVisible();
+  console.log('No quote available ❌');}
   
-  if (noDataElement==true) {
-    console.log('No Quotes available');
-  } else {
-    // execute checkout flow
-        console.log('Quotes available');
+  else {
+  console.log('Quote available ✅');
+  // execute checkout flow
+    await page.locator('//span[normalize-space()="Add To Cart"]').click();
+    // Assuming the popup contains text "Quote Added to Cart"
+    const quotePopup = page.getByText('Quote Added to Cart');
+
+    // Wait for it to be visible (up to 10 seconds)
+    await expect(quotePopup).toBeVisible({ timeout: 30000 });
+
+    await page.getByRole('button', { name: 'Go To Cart' }).click();
+    await page.waitForURL(/cart\.html/, { waitUntil: 'domcontentloaded' });
+
+    await page.getByRole('button', { name: 'Checkout' }).click();
+    await page.waitForURL(/addresses\.html/, { waitUntil: 'domcontentloaded' });
+
+    await page.getByRole('button', { name: 'Proceed to Shipping Method' }).click();
+    await page.waitForURL(/shipping\.html/, { waitUntil: 'domcontentloaded' });
+
+    //await page.goto('https://stage-shop.phenomenex.com/au/en/shipping.html');
+    await page.getByRole('button', { name: 'Proceed to Payment' }).click();
+    await page.waitForURL(/payment\.html/, { waitUntil: 'domcontentloaded' });
+
+    await page.evaluate(() => { window.scrollBy(0, 500);});
+    await page.getByText('Use Card').nth(0).click();
+    //await page.getByRole('button', { name: 'Use Card' }).nth(0).click();
+    await page.evaluate(() => { window.scrollBy(0, 700);});
+    await page.getByRole('checkbox').scrollIntoViewIfNeeded();
+
+    await page.locator('(//input[@id="accept-term"])[2]').check();
+    await page.getByRole('button', { name: 'Place your order' }).click();
+    await page.waitForURL(/receipt\.html/, { waitUntil: 'domcontentloaded' });
+    await expect(page).toHaveURL(/^https:\/\/stage-shop\.phenomenex\.com\/eu\/en\/receipt\.html/);
+
   }
+  page.close();
 });
