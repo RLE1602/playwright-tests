25044ab2fc44202c3fe0dc78ca6306938341210b
Sonu Katkar <sonu.katkar@dhlscontractors.com>
2026-02-20 00:18:10 +0530

    Regression

--- tests/Regression/CoreScenarios/Portugal-06.spec.js
+++ tests/Regression/CoreScenarios/Portugal-06.spec.js
@@ -15,7 +15,7 @@ test('Portugal-06 Existing customer order from approved quote, payment method-cr
   //const noDataElement = page.getByText('No Data');
   const noDataElement = await page.getByRole('button', { name: 'REQUEST A QUOTE' }).isVisible();
   
-  if (noDataElement==false) {
+  if (noDataElement==true) {
     console.log('No Quotes available');
   } else {
     // execute checkout flow
