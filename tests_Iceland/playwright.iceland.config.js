7013d7f18e8b3bb1f299b3feb96189f2a1842187
Sonu Katkar <sonu.katkar@dhlscontractors.com>
2026-02-24 16:14:05 +0530

    Iceland_Regression

--- tests_Iceland/playwright.iceland.config.js
+++ tests_Iceland/playwright.iceland.config.js
@@ -14,7 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   //testDir: './tests',
-    testDir: 'tests/Iceland_Regression/CoreScenarios',
+    testDir: 'tests_Iceland/Iceland_Regression/CoreScenarios',
     timeout: 300_000,
 
   
