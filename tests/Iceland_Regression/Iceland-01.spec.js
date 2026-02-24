a46d11b038bcd4dfaabcbdd9aa0786e1c453239e
Sonu Katkar <sonu.katkar@dhlscontractors.com>
2026-02-24 17:00:36 +0530

    tests

--- tests/Iceland_Regression/Iceland-01.spec.js
+++ tests/Iceland_Regression/Iceland-01.spec.js
@@ -52,6 +52,6 @@ test('I-01 Existing customer order placement , payment method - credit card ( Ma
   await page.getByRole('button', { name: 'Place your order' }).click();
   await page.waitForURL(/receipt\.html/, { waitUntil: 'domcontentloaded' });
   await expect(page).toHaveURL(/^https:\/\/stage-shop\.phenomenex\.com\/uk\/en\/receipt\.html/);
-  await expect(page).toHaveText(/Order Confirmed/i);
+  await expect(page).toHaveText(/Order Confirmed/i, {waitUntil: 'domcontentloaded'});
 
 });
