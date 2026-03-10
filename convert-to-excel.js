e19bc18304aa6f8cb59ec2b03d404b30ed96aa96
Sonu Katkar <sonu.katkar@dhlscontractors.com>
2026-03-10 20:06:11 +0530

    convert-to-excel.js

--- convert-to-excel.js
+++ convert-to-excel.js
@@ -9,7 +9,7 @@ try {
   // 🔹 🔥 UPDATE THESE 3 VALUES
   const repoOwner = "RLE1602";
   const repoName = "playwright-tests";
-  const commitHash = "06404f5657729ea4c49cf32e9a2a3b83504348c9";
+  const commitHash = process.env.GITHUB_SHA;
 
   if (!fs.existsSync(jsonFile)) {
     console.warn('⚠ test-results.json not found. Excel will be empty.');
